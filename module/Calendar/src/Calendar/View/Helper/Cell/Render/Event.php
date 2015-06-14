16a3efed2f501adc2ac09e4d71ea3d4f6f13e5e8
Tobias Krebs <t.krebs@ep-3.de>
2015-06-14 17:46:54 +0200

    Fix event administration for assist users

--- module/Calendar/src/Calendar/View/Helper/Cell/Render/Event.php
+++ module/Calendar/src/Calendar/View/Helper/Cell/Render/Event.php
@@ -11,7 +11,7 @@ class Event extends AbstractHelper
     {
         $view = $this->getView();
 
-        if ($user && $user->can('admin.events')) {
+        if ($user && $user->can('admin.event')) {
             return $view->calendarCellRenderEventForPrivileged($event);
         } else {
             $eid = $event->need('eid');
